692587076efa9f586106b8ba02faf8803f5a9ea9
Andrew Bonas <aabonas@hotmail.com>
2021-05-22 11:20:07 -0700

    initial commit

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Casual', // <title>
-  manifestName: 'Casual',
-  manifestShortName: 'Landing', // max 12 characters
+  siteTitle: 'Harbour Health and Wellness', // <title>
+  manifestName: 'Harbour Health',
+  manifestShortName: 'Harbour', // max 12 characters
   manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
+  manifestBackgroundColor: '#F7F7F9',
+  manifestThemeColor: '#F7F7F9',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-casual/`, // This path is subpath of your hosting https://domain/portfolio
-  heading: 'Gatsby Starter Casual',
-  subHeading: 'A Free Bootstrap 4 based Gatsby Starter',
+  heading: 'Harbour Health and Wellness',
+  subHeading: '.',
 };
